2c7271bca9b15bc28467e838980c02fdc6a3b3b0
Kevin Burleigh <klb@kindling>
2019-07-07 03:12:08 -0500

    configure DatabaseCleaner

--- tester/spec/rails_helper.rb
+++ tester/spec/rails_helper.rb
@@ -7,6 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'database_cleaner'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -31,13 +33,40 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  ##
+  ## See https://github.com/DatabaseCleaner/database_cleaner
+  ##
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:example) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  ##
+  ## Handle situations where the default :transaction cleaning
+  ## strategy interferes with code that creates transactions,
+  ## resulting in nested transactions.
+  ##
+  config.before(:context, truncation: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:example, truncation: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
